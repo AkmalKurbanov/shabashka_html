207e540df7f1b2667d60e4d204ac54a5d8347786
AkmalKurbanov <250296a@gmail.com>
2019-02-14 03:47:55 +0600

    zaebalsya 3:47

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,60 @@
+$(document).ready(function () {
 
+  // header
+
+  $(window).scroll(function () {
+
+    if ($(this).scrollTop() > 400) {
+      $('.header').fadeOut();
+    } else {
+      $('.header').fadeIn();
+    }
+  });
+
+  // header end
+
+
+
+  // mainSlider
+
+  var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+  // mainSlider end
+
+
+   // scroll button
+   function scrollToSection(event) {
+     event.preventDefault();
+     var $section = $($(this).attr('href'));
+     $('html, body').animate({
+       scrollTop: $section.offset().top
+     }, 700);
+   }
+   $('[data-scroll]').on('click', scrollToSection);
+   // scroll button end
+
+  // hamburger
+  $('.hamburger').click(function () {
+    $(this).toggleClass('openClose');
+    $('.mnu-js').toggleClass('mobileMnu-js');
+    $('.hamburger__line').toggleClass('lineBg');
+  });
+
+  $('.mnu-js').click(function () {
+    $(this).removeClass('mobileMnu-js');
+    $('.hamburger').removeClass('openClose');
+    $('.hamburger__line').removeClass('lineBg');
+  });
+  // hamburger end
+
+
+
+
+
+});
